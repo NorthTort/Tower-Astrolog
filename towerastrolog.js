d1c1546948ec637bae83e36c5a569405fe526991
NordTort <41715818+NorthTort@users.noreply.github.com>
2019-05-22 23:11:13 +0300

    Update towerastrolog.js

--- towerastrolog.js
+++ towerastrolog.js
@@ -20,7 +20,14 @@ var prefix = '^'
      		message.channel.sendMessage(message.content.slice(prefix.length).split('say'));
 
 	 }
- 
+  	 if(message.content.startsWith(prefix + 'send')) {
+  if(message.author.id !== '435498881040777231') return;
+    const args = message.content.slice(prefix.length).split('send'); 
+	  message.delete()
+  .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+  .catch(console.error); 
+     		message.channel.sendMessage(message.content.slice(prefix.length).split('send'));
+	 }
  });
 
 client.login(process.env.BOT_TOKEN);
